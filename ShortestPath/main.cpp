c628326565e72276db6828f7012b79f2267f3f71
Eduard <terriblegoodday@icloud.com>
2020-01-17 20:29:50 +1000

    Initial Commit

---
+++ ShortestPath/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShortestPath
+//
+//  Created by Eduard Dzhumagaliev on 1/17/20.
+//  Copyright © 2020 Eduard Dzhumagaliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
